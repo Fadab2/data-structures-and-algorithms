321b2e2e0cd4a25608ab09b0157347a8df28d752
Edris <ali_berg1@yahoo.com>
2022-01-30 14:58:36 -0800

    updated brackets unit test

--- python/tests/test_validate_brackets.py
+++ python/tests/test_validate_brackets.py
@@ -1,14 +1,28 @@
 from code_challenges.validate_brackets.validate_brackets import validate_brackets
-import pytest
 
-assert validate_brackets("") == True
-assert validate_brackets("{}") == True
-assert validate_brackets("()[[Extra Characters]]") == True
-assert validate_brackets("{}{Code}[Fellows](())") == True
+def test_no_brackets():
+    assert validate_brackets("") == True
 
 
+def test_one_valid_brackets():
+    assert validate_brackets("{}") == True
 
-assert validate_brackets("[({}]") == False
-assert validate_brackets("{(})") == False
-assert validate_brackets("{") == False
+def test_complex_valid_brackets():
+    assert validate_brackets("()[[Extra Characters]]") == True
+
+
+def test_text_complex_brackets():
+    assert validate_brackets("{}{Code}[Fellows](())") == True
+
+
+def test_invalid1_brackets():
+    assert validate_brackets("[({}]") == False
+
+
+def test_invalid2_brackets():
+    assert validate_brackets("{(})") == False
+
+
+def test_invalid_single_brackets():
+    assert validate_brackets("{") == False
 
