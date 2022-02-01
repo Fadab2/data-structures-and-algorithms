125c756011c5d563e66749904f945aa517c4cc9c
Edris <ali_berg1@yahoo.com>
2022-01-31 20:52:21 -0800

    added unit test and fixed add function

--- python/code_challenges/trees/binary_search_tree.py
+++ python/code_challenges/trees/binary_search_tree.py
@@ -5,58 +5,72 @@ from code_challenges.trees.node import Node
 
 class BinarySearchTree(BinaryTree):
     def __init__(self, root=None):
-        super().__init__(root)
+         self.root = root
+
 
     def add(self, value):
-
-        def find_and_add(root, value):
             new_node = Node(value)
-            if self.root is None:
+            current = self.root
+            if current is None:
                 self.root = new_node
-                return
+                #return
+            while current:
 
-            if root.value < new_node.value:
-                if root.right:
-                    find_and_add(root.right, value)
+                if value > current.value:
+                    if current.right == None:
+                        current.right = new_node
+                        return self
+                    current = current.right
                 else:
-                    root.right = new_node
-
-            if self.root > new_node.value:
-                if root.left:
-                    find_and_add(root.left, value)
-                else:
-                    root.left = new_node
-
-            find_and_add(self.root, value)
-
+                    if current.left == None:
+                        current.left = new_node
+                        return self
+                    current = current.left
 
     def contains(self, value):
+
         if self.root is None:
             return False
-
         if self.root.value == value:
             return True
 
         if value < self.root.value:
-            if self.root.left:
-                contains(self.root.left, value)
-                return True
+            return contains(self.root.left, value)
         else:
-            if self.root.right:
-                contains(self.root.right, value)
-                return True
-
-        return False
+            return contains(self.root.right, value)
+            #return contains(self.root, value)
 
 
-if __name__ == "__main__":
-   bst = BinarySearchTree(Node(50))
-   bst.add(Node(40))
-   bst.add(Node(30))
-   bst.add(Node(45))
-   bst.add(Node(60))
-   bst.add(Node(55))
-   bst.add(Node(70))
+# if __name__ == "__main__":
+#    bst = BinarySearchTree(Node(50))
+#    bst.add(Node(40))
+#    bst.add(Node(30))
+#    bst.add(Node(45))
+#    bst.add(Node(60))
+#    bst.add(Node(55))
+#    bst.add(Node(70))
 
 
-   print(bst.post_order)
+#    print(bst.post_order)
+
+#  def add(self, value):
+
+#         def find_and_add(root, value):
+#             new_node = Node(value)
+#             if self.root is None:
+#                 self.root = new_node
+#                 #return
+
+#             if root.value < new_node.value:
+#                 if root.right:
+#                     find_and_add(root.right, value)
+#                 else:
+#                     root.right = new_node
+
+#             if self.root > new_node.value:
+#                 if root.left:
+#                     find_and_add(root.left, value)
+#                 else:
+#                     root.left = new_node
+
+#             find_and_add(self.root, value)
